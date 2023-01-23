adfdb2942a20e65c8a8b2de8f6751bb0e07b9043
berger <robertberger.dev@gmail.com>
2023-01-23 17:57:43 -0500

    finished the starter code

--- index.ts
+++ index.ts
@@ -24,3 +24,18 @@ interface Square {
     length: number;
     color?: string;
 }
+
+function useSquare(mySquare: Square): string {
+    let message = "The ";
+
+    if (mySquare.color) {
+        message += `${mySquare.color} `;
+    }
+
+    message += `square has a perimeter of ${mySquare.length * 4}`;
+
+    return message;
+}
+
+console.log(useSquare({ length: 3, color: 'red' }));
+console.log(useSquare({ length: 5 }));
