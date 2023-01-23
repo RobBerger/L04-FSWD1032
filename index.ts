c891685030d14d2b393bc012e949c8e5b6d43d57
berger <robertberger.dev@gmail.com>
2023-01-23 17:53:29 -0500

    commented out all code in index.ts

--- index.ts
+++ index.ts
@@ -1,21 +1,22 @@
-interface nameInterface {
-    name: string;
-    prettyPrint(): void;
-}
+// interface nameInterface {
+//     name: string;
+//     prettyPrint(): void;
+// }
 
-function namePrint(givenObject: nameInterface){
-    console.log(givenObject.name);
-}
+// function namePrint(givenObject: nameInterface){
+//     console.log(givenObject.name);
+// }
 
-let myObject = {
-    name: "Elmo", address: "123 Sesame St.",
-    prettyPrint: function() {
-        console.log(`Name: ${this.name}`);
-    }
-    };
+// let myObject = {
+//     name: "Elmo", address: "123 Sesame St.",
+//     prettyPrint: function() {
+//         console.log(`Name: ${this.name}`);
+//     }
+//     };
 
-    function buildName(givenObject: nameInterface) {
-        givenObject.prettyPrint();
-    }
+//     function buildName(givenObject: nameInterface) {
+//         givenObject.prettyPrint();
+//     }
     
-    buildName(myObject);
+//     buildName(myObject);
+
