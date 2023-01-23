ca208cf9de436b7ae927fc42012988d33967f3ca
berger <robertberger.dev@gmail.com>
2023-01-23 18:01:46 -0500

    commented out previous code

--- index.ts
+++ index.ts
@@ -20,22 +20,22 @@
     
 //     buildName(myObject);
 
-interface Square {
-    length: number;
-    color?: string;
-}
+// interface Square {
+//     length: number;
+//     color?: string;
+// }
 
-function useSquare(mySquare: Square): string {
-    let message = "The ";
+// function useSquare(mySquare: Square): string {
+//     let message = "The ";
 
-    if (mySquare.color) {
-        message += `${mySquare.color} `;
-    }
+//     if (mySquare.color) {
+//         message += `${mySquare.color} `;
+//     }
 
-    message += `square has a perimeter of ${mySquare.length * 4}`;
+//     message += `square has a perimeter of ${mySquare.length * 4}`;
 
-    return message;
-}
+//     return message;
+// }
 
-console.log(useSquare({ length: 3, color: 'red' }));
-console.log(useSquare({ length: 5 }));
+// console.log(useSquare({ length: 3, color: 'red' }));
+// console.log(useSquare({ length: 5 }));
